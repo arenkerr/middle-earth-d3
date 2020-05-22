fe3aa8dfeb99845c72f838917f3c82e312ccc672
Alana Kerr <ani.kerr@gmail.com>
2020-05-22 13:58:13 -0400

    deploy: apollo client uri

--- src/apollo.tsx
+++ src/apollo.tsx
@@ -1,5 +1,5 @@
 import ApolloClient from 'apollo-boost';
 
 export default new ApolloClient({
-    uri: 'http://localhost:4000/graphql',
+    uri: process.env.NODE_ENV === 'production' ? 'http://middle-earth-trees.herokuapp.com/graphql' : 'http://localhost:4000/graphql',
 });
