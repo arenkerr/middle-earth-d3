3c1475e9a01f73924b77036fcf2867f2cc72723b
Alana Kerr <ani.kerr@gmail.com>
2020-05-22 16:10:43 -0400

    deploy: apollo server options

--- server/server.js
+++ server/server.js
@@ -13,12 +13,19 @@ const resolvers = require('./resolvers')
 
 
 // Set up Express server
-const server = new ApolloServer({ typeDefs, resolvers })
+const server = new ApolloServer({ 
+  introspection: true,
+  playground: true,
+  typeDefs, 
+  resolvers 
+})
 const app = express();
 app.use(cors());
 server.applyMiddleware({ app });
 
-app.listen({ port: process.env.PORT || 4000 }, () =>
+const port = process.env.PORT || 4000;
+
+app.listen(port, () =>
   console.log(`🚀 Server ready on port: ${process.env.PORT || 4000}`)
 );
 
