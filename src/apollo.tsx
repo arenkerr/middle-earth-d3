cabe80f4238e7c4dfe2cfc80592be82bc34af12c
Alana Kerr <ani.kerr@gmail.com>
2020-05-22 18:11:37 -0400

    fix: move server directory, use deployed server

--- src/apollo.tsx
+++ src/apollo.tsx
@@ -1,5 +1,5 @@
 import ApolloClient from 'apollo-boost';
 
 export default new ApolloClient({
-    uri: process.env.NODE_ENV === 'production' ? 'http://middle-earth-trees.herokuapp.com/graphql' : 'http://localhost:4000/graphql',
+    uri: 'http://middle-earth-server.herokuapp.com/graphql',
 });
