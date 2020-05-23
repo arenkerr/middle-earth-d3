630512a84faefb7c0fec17cbb9cffcab0a9430fd
Alana Kerr <ani.kerr@gmail.com>
2020-05-23 19:53:03 -0400

    fix: add node env check to apollo client

--- src/apollo.tsx
+++ src/apollo.tsx
@@ -1,3 +1,6 @@
 import ApolloClient from 'apollo-boost';
 
-export default new ApolloClient({ uri: 'https://middle-earth-server.herokuapp.com/graphql' });
+export default new ApolloClient(process.env.NODE_ENV === 'production' ? 
+    { uri: 'https://middle-earth-server.herokuapp.com/graphql' } 
+    : {uri: 'http://localhost:4000/graphql'}
+);
