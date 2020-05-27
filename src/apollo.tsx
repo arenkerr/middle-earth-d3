85c083f28ac86e61e0d380a6e79e9827967a9646
Alana Kerr <ani.kerr@gmail.com>
2020-05-27 17:54:19 -0400

    fix: apollo client

--- src/apollo.tsx
+++ src/apollo.tsx
@@ -2,6 +2,6 @@ import ApolloClient from 'apollo-boost';
 
 export default new ApolloClient({ 
     uri: process.env.NODE_ENV === 'production' ? 
-        process.env.APOLLO_CLIENT : 
+        process.env.REACT_APP_API_URL : 
         'http://localhost:4000/graphql'
 });
